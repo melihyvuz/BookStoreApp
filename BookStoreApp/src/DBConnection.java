3a6eb19e09b8d32d21b9b03a3f23bacbef4aed28
melihyvuz <melih_yvuz@hotmail.com>
2025-04-23 16:17:26 +0300

    Update DBConnection.java

--- BookStoreApp/src/DBConnection.java
+++ BookStoreApp/src/DBConnection.java
@@ -1,9 +1,9 @@
 import java.sql.*;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/BookStoreDB" ;
+    private static final String URL = "jdbc:mysql://localhost:3306/" ;
     private static final String USER = "root";
-    private static final String PASSWORD = "06022004";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
